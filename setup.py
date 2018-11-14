00c64743358e32b1b15a117fd4186a14b8e08069
Maxtasy <maxtasy88@web.de>
2018-11-14 20:57:06 +0100

    Add files via upload
    Added setup.py for .exe building

---
+++ setup.py
@@ -0,0 +1,22 @@
+import sys
+import os.path
+from cx_Freeze import setup, Executable
+
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "Melody",
+        version = "1.0",
+        description = "MP3 player",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("main.py", base=base)])
